f5ccc5050a8fd60b340ea7d326231d3c977c94d1
Bharathi.Valluri <you@example.com>
2022-12-22 17:40:40 +0530

    modified

--- db.js
+++ db.js
@@ -10,12 +10,11 @@ const client = new Client({
 client.connect(function (err) {
   if (err) throw err
   console.log('Connected!')
-  // var sql =
-  //   'CREATE TABLE customers (id SERIAL PRIMARY KEY,name VARCHAR(255), address VARCHAR(255))'
-  // client.query(sql, function (err, result) {
-  //   if (err) throw err
-  //   console.log('Table created')
-  // })
+  var sql =
+    'CREATE TABLE customers (id SERIAL PRIMARY KEY,name VARCHAR(255), address VARCHAR(255))'
+  client.query(sql, function (err, result) {
+    if (err) throw err
+    console.log('Table created')
+  })
 })
 module.exports = { client }
-// module.exports = client
